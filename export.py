06f3fbfd8c5a14aa9fe172f9e1f5442703849ac0
Nam Tong <41351528+namth3061@users.noreply.github.com>
2023-07-06 18:55:44 +0700

    Update export.py

--- export.py
+++ export.py
@@ -1,98 +1,110 @@
-
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import time
-# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+
 class Login:
+    accounts = ['0528695750|T0ngnamm', '0984967115|anhkiem']
+    videoUrl = 'https://tx.app611.net/Game/KULive.aspx?kuUrl=https://gat5.vn3kuvideo.com/'
+    mainUrl = 'https://tx.app611.net/index.aspx'
+    SCREENONE = 1
+    SCREENTWO = 2
+
     def __init__(self):
-        
-        self.loginWeb()
+        self.main()
         # self.getData()
 
-    def loginWeb(self): 
-        usernameStr = 'GEEMINIOS'
-        passwordStr = 'T0ngnamm'
-
+    def main(self):
         browser = webdriver.Chrome()
-        # browser.get(('https://vn2.ku6122.net'))
-        browser.get(('https://tx.app611.net/index.aspx'))
+        browser.get(self.mainUrl)
 
-        # sleep(3)
-        # browser.find_element(By.ID, "loginbutton").click();
-        # sleep(5)
-        username = browser.find_element(By.ID, "txtUser")
-        # username = browser.find_element(By.ID, "accountId")
-        username.send_keys(usernameStr)
+        self.loginWeb(browser, self.accounts[0])
 
-        password = browser.find_element(By.ID, "txtPassword")
-        # password = browser.find_element(By.ID, "accountPwd")
-        password.send_keys(passwordStr)
+        self.switchToWindow(browser, self.SCREENONE)
 
-        # signInButton = browser.find_element(By.ID, "signin")
-        signInButton = browser.find_element(By.CLASS_NAME, "btn_signIn")
-        signInButton.click()
+        print('Click to baccarat_btn')
+        browser.find_element(By.CLASS_NAME, 'baccarat_btn').click()
+        sleep(2)
 
+        print('Getting source...')
+        baccaratCn = browser.find_element(
+            By.ID, 'scroll_pane').get_attribute('outerHTML')
+
+        # start login again
+        self.signOut(browser)
+        self.loginWeb(browser, self.accounts[1])
+        sleep(3)
+        # end login again
+
+        self.switchToWindow(browser, self.SCREENTWO)
+
+        print('Click to BKbaccarat_btn')
+        browser.find_element(By.CLASS_NAME, 'BKbaccarat_btn').click()
+        sleep(2)
+
+        print('Getting source...')
+        baccaratMc = browser.find_element(
+            By.ID, 'scroll_pane').get_attribute('outerHTML')
+
+        print('==========================')
+        print('Start clone data!')
+        print('==========================')
+        
+        self.whileSave(baccaratCn, baccaratMc)
+
+    def loginWeb(self, browser, account):
+        account = account.split('|')
+        print('Login account ' + account[0])
+
+        browser.find_element(By.ID, 'txtUser').send_keys(account[0])
+        browser.find_element(By.ID, 'txtPassword').send_keys(account[1])
+
+        sleep(3)
+        browser.find_element(By.CLASS_NAME, 'btn_signIn').click()
+        
         sleep(5)
-        browser.execute_script("window.open('');")
-        # 
-        browser.switch_to.window(browser.window_handles[1])
-        # browser.get(('https://tx.app611.net/Game/KULive.aspx'))
-        browser.get(('https://vn2.ku6122.net/CheckGame?gameType=BB_LiveGame&subGameType=&dt='+usernameStr+time.time()))
-        # # browser.execute_script('''window.open("https://tx.app611.net/Game/KULive.aspx?kuUrl=https://gat5.vn3kuvideo.com/", "_blank");''')
-        # sleep(10)
-        # browser.find_element(By.CLASS_NAME, "flagshipNav_btn").click()
-        # sleep(3)
 
-        # #start login again
-        # browser.switch_to.window(browser.window_handles[0])
-        # browser.find_element(By.CLASS_NAME, "btn_memSignOut").click()
-        # sleep(3)
-        # browser.find_element(By.ID, "txtUser").send_keys(usernameStr)
-        # browser.find_element(By.ID, "txtPassword").send_keys(passwordStr)
-        # browser.find_element(By.CLASS_NAME, "btn_signIn").click()
-        # #end login again
+    def signOut(self, browser):
+        print('Signing out...')
+        browser.switch_to.window(browser.window_handles[0])
+        browser.find_element(By.CLASS_NAME, 'btn_memSignOut').click()
+        sleep(3)
 
-        # browser.execute_script("window.open('');")
-        # browser.switch_to.window(browser.window_handles[2])
-        # # browser.execute_script('''window.open("https://tx.app611.net/Game/KULive.aspx?kuUrl=https://gat5.vn3kuvideo.com/", "_blank");''')
-        # browser.get("https://tx.app611.net/Game/KULive.aspx")
-        # sleep(10)   
-        # # browser.find_element(By.CLASS_NAME, "baccarat_btn").click()
-        # # sleep(3)
-   
-        i=0
-        while(True):
-            i=i+1
-            try: 
-                print('incoming')
-                # scrollPane = browser.find_element(By.ID, "scroll_pane").text
-                # scrollPane = browser.find_element(By.ID, "scroll_pane")
-                # if scrollPane:
-                #     source_code = scrollPane.get_attribute("outerHTML")
-                #     self.getData(source_code)
+    def switchToWindow(self, browser, screenNumber):
+        print('Switch to window bet')
+        browser.execute_script('window.open('');')
+        browser.switch_to.window(browser.window_handles[screenNumber])
+        browser.get(self.videoUrl)
+        sleep(15)
 
+    def whileSave(self, baccaratCn, baccaratMc):
+        i = 0
+        while (True):
+            i = i+1
+            try:
+                self.saveData(baccaratCn, 'cn')
+                self.saveData(baccaratMc, 'mc')
+                seconds = 5
+                while seconds:
+                    mins, secs = divmod(seconds, 60)
+                    timer = '{:02d}:{:02d}'.format(mins, secs)
+                    print("Sleeping in " + timer, end="\r")
+                    time.sleep(1)
+                    seconds -= 1
             finally:
-                print('not found')
-                
-            # sleep(10)
-            sleep(5)
+                print('Crawler file successfully!')
             pass
 
-    def getData(self, source): 
-        soup = BeautifulSoup(source, "html.parser")
+    def saveData(self, source, type):
+        self.append_new_line('./public/domHTML/'+type+".html", source)
 
-        elements = soup.find_all("div", class_="game_box".split())
-        print(elements)
-
-        # print("\n".join("{} {}".format(el['class'], el.get_text()) for el in elements))
-        
+    def append_new_line(self, file_name, source):
+        with open(file_name, "wb") as file:
+            file.write(source.encode())
 
 if __name__ == "__main__":
     login = Login()
